a854628167950d57f07a590294414a3f0226688f
Gabriel Henrique <102603536+GabrielV26@users.noreply.github.com>
2024-08-07 18:34:41 -0300

    Refatoração da views para utilizar orientação a objetos

--- RomanNumeral/views.py
+++ RomanNumeral/views.py
@@ -8,94 +8,88 @@ import json
 import re
 
 
+class RomanConverter:
+    ROMAN_NUMERALS = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
+    }
+
+    ROMAN_NUMERAL_PATTERNS = [
+        ('M', 1000), ('CM', 900), ('D', 500), ('CD', 400), ('C', 100), ('XC', 90),
+        ('L', 50), ('XL', 40), ('X', 10), ('IX', 9), ('V', 5), ('IV', 4), ('I', 1)
+    ]
+
+    @staticmethod
+    def is_valid_roman(roman):
+        pattern = r"^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$"
+        return re.match(pattern, roman) is not None
+
+    def roman_to_number(self, roman_numeral):
+        if not self.is_valid_roman(roman_numeral.upper()):
+            return None
+
+        add_values = 0
+        rem_values = 0
+        old_value = 0
+
+        for char in reversed(roman_numeral):
+            value = self.ROMAN_NUMERALS[char.upper()]
+
+            if value > add_values or value == old_value:
+                old_value = value
+                add_values += value
+            elif value < add_values:
+                rem_values += value
+
+        return add_values - rem_values
+
+    def number_to_roman(self, number):
+        roman_string = ""
+        for numeral, value in self.ROMAN_NUMERAL_PATTERNS:
+            while number >= value:
+                roman_string += numeral
+                number -= value
+
+        return roman_string
+
+
+class RealConverter:
+    def __init__(self):
+        self.roman_converter = RomanConverter()
+
+    def roman_real_to_real(self, value_for_converted):
+        if isinstance(value_for_converted, str):
+            try:
+                parte1, parte2 = value_for_converted.split(',', 1)
+                return f"R${self.roman_converter.roman_to_number(parte1)}.{self.roman_converter.roman_to_number(parte2)}"
+            except ValueError:
+                return f"R${self.roman_converter.roman_to_number(value_for_converted)}"
+        return None
+
+    def real_to_roman_real(self, value_for_converted):
+        if isinstance(value_for_converted, float):
+            text_convert = str(value_for_converted)
+            parte1, parte2 = text_convert.split('.', 1)
+            return f"{self.roman_converter.number_to_roman(int(parte1))}.{self.roman_converter.number_to_roman(int(parte2))}"
+        else:
+            return self.roman_converter.number_to_roman(value_for_converted)
+
+
 def index(request):
     form = DataInputForm()
     return render(request, 'index.html', {'form': form})
 
 
-# Expressão regular para que aceita apenas números romanos
-def is_valid_roman(roman):
-    pattern = r"^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$"
-    return re.match(pattern, roman) is not None
-
-
-# Converte numero romano para inteiro
-def number_for_roman(roman_numeral):
-    add_values = 0
-    rem_values = 0
-    old_value = 0
-
-    roman_numbers = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-
-    if is_valid_roman(roman_numeral.upper()):
-        letter = reversed(list(roman_numeral))
-
-        for char in letter:
-            value = roman_numbers[char.upper()]
-
-            if value > add_values or value == old_value:
-                old_value = value
-                add_values = add_values + value
-            elif value < add_values:
-                rem_values = rem_values + value
-
-        roman_numeral = add_values - rem_values
-        return roman_numeral
-
-
-# Converte "real romano" para real
-def number_for_roman_real(value_for_converted):
-    result = ''
-    if isinstance(value_for_converted, str):
-        try:
-            parte1, parte2 = value_for_converted.split(',', 1)
-            result = 'R$' + str(number_for_roman(parte1)) + '.' + str(number_for_roman(parte2))
-        except ValueError:
-            result = 'R$' + str(number_for_roman(value_for_converted))
-
-    return result
-
-
-# Converte de inteiro para romano
-def roman_for_number(number):
-    roman_numbers = [
-        ('M', 1000), ('CM', 900), ('D', 500), ('CD', 400), ('C', 100), ('XC', 90), ('L', 50), ('XL', 40),
-        ('X', 10), ('IX', 9), ('V', 5), ('IV', 4), ('I', 1)
-    ]
-
-    roman_string = ""
-    for numeral, value in roman_numbers:
-        while number >= value:
-            roman_string += numeral
-            number -= value
-
-    return roman_string
-
-
-# Converte o numero "real romano" para numero real
-def roman_for_number_real(value_for_converted):
-    result = ''
-    if isinstance(value_for_converted, float):
-        text_convert = str(value_for_converted)
-        parte1, parte2 = text_convert.split('.', 1)
-        result = str(roman_for_number(int(parte1))) + '.' + str(roman_for_number(int(parte2)))
-    else:
-        roman_for_number(value_for_converted)
-    return result
-
-
-# View do formulário
 def converter(request):
     if request.method == 'POST':
         form = DataInputForm(request.POST)
         if form.is_valid():
             value = form.cleaned_data['value']
+            real_converter = RealConverter()
             if isinstance(value, str):
-                value = number_for_roman_real(value)
+                value = real_converter.roman_real_to_real(value)
             else:
-                value = roman_for_number_real(value)
+                value = real_converter.real_to_roman_real(value)
             resultado = f"Valor convertido: {value}"
             return JsonResponse({'resultado': resultado})
         return JsonResponse({'erro': 'Dados inválidos.'}, status=400)
     return JsonResponse({'erro': 'Método não permitido.'}, status=405)
-
